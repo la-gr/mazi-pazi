d895a36d517830137d430392c6635536b88f6f3d
lara grahek <lara@grahek.net>
2025-04-17 13:35:26 -0400

    finished most ui stuff

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+import i18next from 'https://unpkg.com/i18next@latest/dist/esm/i18next.js';
+
 let bookGr = document.getElementById("grb");
 let bookSit = document.getElementById("sb");
 let moreGr = document.getElementById("grm");
 let moreSit = document.getElementById("sm");
+let srb = document.getElementById("srb");
+let eng = document.getElementById("eng");
 
 //resets the form with message sending when the page is reloaded
 window.onload = function() {
@@ -13,10 +17,12 @@ window.onload = function() {
 moreGr.addEventListener("click", (e) => {
     window.location.href = "more info.html";
     sessionStorage.setItem("selection", "groom");
+    sessionStorage.setItem("lang", currentLang);
 })
 moreSit.addEventListener("click", (e) => {
     window.location.href = "more info.html";
     sessionStorage.setItem("selection", "sit");
+    sessionStorage.setItem("lang", currentLang);
 })
 
 //booking
@@ -27,10 +33,135 @@ bookSit.addEventListener("click", (e) => {
     window.location.href = "book.html";
 })
 
-function createReview(starNum){
-    //create number of stars for the review
+//language buttons
+let currentLang = "en";
+srb.addEventListener("click", async() => {
+    translate('sr');
+    changeBut(srb,eng);
+    currentLang = "sr";
+})
+eng.addEventListener("click", async() => {
+    translate('en');
+    changeBut(eng,srb);
+    currentLang = "en";
+})
 
-    //create reviewer name and review message
+i18next.init({
+    lng: currentLang,
+    fallbackLng: 'en',
+    resources: {
+        en: {
+            translation: {
+                titleAbout: 'About Us',
+                aboutMe: 'Mazi Pazi has 10 years of love, care, and patience for caring for your furry friends. Whether it\'s their recreation, hygiene and hair cuts, or having a comfortable and safe environment to vacation to. An environment where they have all the same benefits that they have at home - such as sleeping on their own pillow or bed with backyard access at all times.',
+                ques: 'Contact Me!',
+                name: 'Name',
+                message: 'Message',
+                send: 'Send',
+                grooming: 'Grooming',
+                daycare: 'Daycare/Boarding',
+                moreInfo: 'MORE INFO',
+                book: 'BOOK',
+                boarding: 'Daycare and Boarding'
+            }
+        },
+        sr: {
+            translation: {
+                titleAbout: 'O Nama',
+                aboutMe: 'Mazi Pazi se 10god sa ljubavlju, paznom i strpljenjem brine o vasim ljubimicima bilo da rec o njihovoj higijeni i frizuri bilo da je u pitanju njihov boravak u prijatnom i sigurnom okruzenju gde imaju sve pogodnosti kao kod kuce-spavanje na svom jastucetu ili krevetu uz konstantan pristup dvoristu.',
+                ques: 'Kontaktirajte Me!',
+                name: 'Ime',
+                message: 'Poruku',
+                send: 'Posalji',
+                grooming: 'Negovanje',
+                daycare: 'Boravka/Pansiona',
+                moreInfo: 'VISE INFO',
+                book: 'ZAKAZI',
+                boarding: 'Boravka i Pansiona'
+            }
+        }
+    }
+})
+
+function translate (la){
+    document.getElementById("title-about").innerHTML = i18next.t("titleAbout", {lng: la});
+    document.getElementById("about-me").innerHTML = i18next.t("aboutMe", {lng: la});
+    document.getElementById("ques").innerHTML = i18next.t("ques", {lng: la});
+    document.getElementById("name").placeholder = i18next.t("name", {lng: la});
+    document.getElementById("message").placeholder = i18next.t("message", {lng: la});
+    document.getElementById("send").textContent = i18next.t("send", {lng: la});
+    document.getElementById("grooming").innerHTML = i18next.t("grooming", {lng: la});
+    document.getElementById("daycare").innerHTML = i18next.t("daycare", {lng: la});
+    document.getElementById("grm").innerHTML = i18next.t("moreInfo", {lng: la});
+    document.getElementById("sm").innerHTML = i18next.t("moreInfo", {lng: la});
+    document.getElementById("grb").innerHTML = i18next.t("book", {lng: la});
+    document.getElementById("sb").innerHTML = i18next.t("book", {lng: la});
+    document.getElementById("board").textContent = i18next.t("boarding", {lng: la});
+    document.getElementById("groom").textContent = i18next.t("grooming", {lng: la});
+}
+//changes the button appearance
+function changeBut(active, not){
+    not.style.zIndex = "1";
+    active.style.zIndex = "2";
+    active.style.backgroundColor = "#e9b4ff";
+    not.style.backgroundColor = "#a889a9";
 }
 
-createReview(3); //starNum will be replaced with actual eventually
+//shrinking header on scroll
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        document.getElementById("ticky").style.backgroundSize = "100% 40%";
+        document.getElementById("mazi").style.scale = ".75";
+        document.getElementById("mazi").style.top = "-5%";
+        document.getElementById("daycare").style.top = "24%";
+        document.getElementById("grooming").style.top = "24%";
+    } else {
+        document.getElementById("ticky").style.backgroundSize = "100% 50%";
+        document.getElementById("mazi").style.scale = "1";
+        document.getElementById("mazi").style.top = "0";
+        document.getElementById("daycare").style.top = "32%";
+        document.getElementById("grooming").style.top = "32%";
+    }
+}
+
+document.getElementById("mazi").onclick = function() {
+    window.location.href = "index.html";
+}
+document.getElementById("grooming").onclick = function() {
+    window.location.href = "more info.html";
+    sessionStorage.setItem("lang", currentLang);
+    sessionStorage.setItem("selection", "groom");
+
+}
+document.getElementById("daycare").onclick = function() {
+    window.location.href = "more info.html";
+    sessionStorage.setItem("lang", currentLang);
+    sessionStorage.setItem("selection", "sit");
+}
+
+const slideIndices = {}; // stores an index per class so that each slideshow runs independently
+
+function startSlideshow(className, interval) {
+    slideIndices[className] = 0;
+    const slides = document.getElementsByClassName(className);
+
+    function showSlides() {
+        let slides = document.getElementsByClassName(className);
+        for (let i = 0; i < slides.length; i++) {
+            slides[i].classList.remove("active");
+        }
+        slideIndices[className]++;
+        if (slideIndices[className] > slides.length) {
+            slideIndices[className] = 1;
+        }
+        slides[slideIndices[className] - 1].classList.add("active");
+        setTimeout(showSlides, interval); // Change image every 2 seconds
+    }
+    showSlides();
+}
+
+startSlideshow("DogsGr", 3400);
+startSlideshow("DogsDec", 3000);
+startSlideshow("person", 3500);
--- more info.js
+++ more info.js
@@ -1,38 +1,47 @@
-let leftBut = document.getElementById("left");
-let rightBut = document.getElementById("right");
+
 let info = document.getElementById("info");
 let about = document.getElementById("about");
+let prices = document.getElementById("prices");
+let dogCont = document.querySelector('.dog');
+let dogNav = document.querySelector('.dogNav');
 
-let img1 = document.getElementById("img1");
-let img2 = document.getElementById("img2");
-let img3 = document.getElementById("img3");
-let lil1 = document.getElementById("lil1");
-let lil2 = document.getElementById("lil2");
-let lil3 = document.getElementById("lil3");
+let textGr = ["DOG GROOMING", "this is a description for the grooming service that this business provides wow wow i don't know what to put here there are a lot of things that could be said", "Bathing - $<br>Brushing - $<br>Hair Cut - $<br>Nail Clipping - $<br>Ear Cleaning - $<br>Hygienic Treatment - $<br>Anti-Flea and Anti-Tick Treatment - $<br>Medicinal Shampoo Treatment - $", "NEGOVANJE PASA", "ovo je diskripcija neka", "Kupanje - $<br>Rascetkavanje - $<br>Sisanje - $<br>Skracivanje Noktiju - $<br>Ciscenje Usiju - $<br>Higijenski Tretman - $<br>Tretman Protiv Buva i Krpelja - $<br>Tretmen Medicinskim Samponima - $"  ];
+let textSit = ["DOG DAYCARE/BOARDING", "this is a description for the sitting service that this business provides wow wow i don't know what to put here there are a lot of things that could be said", "Daycare - $<br>Multi-Day Boarding - $", "BORAVKA/PANSIONA", "ima puno informacija ovde", "Uskluga Dnevnog Boravka - $<br>Usluga Visednevnog Pansiona - $"];
 //determining whether user chose grooming or sitting
+let current = "";
 const selection = sessionStorage.getItem("selection");
+const language = sessionStorage.getItem("lang");
 if (selection === "groom") {
     //description of the service selected
-    about.innerHTML = "GROOMING";
-    info.innerHTML = "this is a description for the grooming service that this business provides wow wow i don't know what to put here there are a lot of things that could be said";
-    //setting the images to dog grooming pictures
-    img1.src = "dogGR.png";
-    img2.src = "dog2.png";
-    img3.src = "dog3.png";
-    lil1.src = "dogGR.png";
-    lil2.src = "dog2.png";
-    lil3.src = "dog3.png";
+    current = textGr;
+    if (language === "en"){
+        english()
+    }else{
+        serbian()
+    }
+    setImages("groom");
 } else if (selection === "sit") {
     //description of the service selected
-    about.innerHTML = "SITTING";
-    info.innerHTML = "this is a description for the sitting service that this business provides wow wow i don't know what to put here there are a lot of things that could be said";
-    //setting the images to dog sitting images
-    img1.src = "area1.png";
-    img2.src = "area2.png";
-    img3.src = "area3.png";
-    lil1.src = "area1.png";
-    lil2.src = "area2.png";
-    lil3.src = "area3.png";
+    current = textSit;
+    if (language === "en"){
+        english()
+    }else{
+        serbian()
+    }
+    setImages("stay");
+}
+
+//sets the images for the slideshow
+function setImages(type){
+    for (let i = 1; i < 6; i++) {
+        let dog = document.createElement("img");
+        let littleImg = document.createElement("img");
+        dog.src = "dogs/"+type+i+".jpg";
+        littleImg.src = "dogs/"+type+i+".jpg";
+        littleImg.onclick = () => {scrollToImage(i-1)}
+        dogCont.appendChild(dog);
+        dogNav.appendChild(littleImg);
+    }
 }
 
 //slides images from one to another
@@ -43,4 +52,59 @@ function scrollToImage(index) {
         left: index * width,
         behavior: 'smooth'
     });
+}
+
+//changes languages from serbian to english and vice versa, along with button color
+let currentLang = language
+srb.addEventListener("click", (e) => {
+    changeBut(srb,eng);
+    currentLang = "srb";
+    serbian();
+})
+eng.addEventListener("click", (e) => {
+    changeBut(eng,srb);
+    currentLang = "en";
+    english()
+})
+
+function english(){
+    about.innerHTML = current[0];
+    info.innerHTML = current[1];
+    prices.innerHTML = current[2];
+    document.getElementById("book").textContent = "Book an Appointment!";
+    document.getElementById("grooming").innerHTML = "Grooming";
+    document.getElementById("daycare").innerHTML = "Daycare/Boarding";
+    changeBut(eng,srb);
+}
+function serbian(){
+    about.innerHTML = current[3];
+    info.innerHTML = current[4];
+    prices.innerHTML = current[5];
+    document.getElementById("book").textContent = "Zakazite Imenovanje!";
+    document.getElementById("grooming").innerHTML = "Negovanje";
+    document.getElementById("daycare").innerHTML = "Boravka/Pansiona";
+    changeBut(srb,eng);
+}
+
+
+function changeBut(active, not){
+    not.style.zIndex = "1";
+    active.style.zIndex = "2";
+    active.style.backgroundColor = "#e9b4ff";
+    not.style.backgroundColor = "#a889a9";
+}
+
+document.getElementById("mazi").onclick = function() {
+    window.location.href = "index.html";
+}
+document.getElementById("grooming").onclick = function() {
+    window.location.href = "more info.html";
+    sessionStorage.setItem("lang", currentLang);
+    sessionStorage.setItem("selection", "groom");
+
+}
+document.getElementById("daycare").onclick = function() {
+    window.location.href = "more info.html";
+    sessionStorage.setItem("lang", currentLang);
+    sessionStorage.setItem("selection", "sit");
 }
